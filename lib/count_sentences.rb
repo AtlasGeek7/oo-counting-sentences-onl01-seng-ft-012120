8ae844785bc99b1115695fa7ff199f65a8606ca5
Khalid T <khalid.tissire@gmail.com>
2020-01-30 08:54:22 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.split('').last === '.'
   end
-
+    
   def question?
-
+    return self.split('').last === '?'
   end
 
   def exclamation?
-
+    return self.split('').last === '!'
   end
 
   def count_sentences
-
+    return self.split(/\.|\?|\!/).length
   end
 end
